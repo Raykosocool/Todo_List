9f32286362e18f07261dfede725cca162e5a5e3b
Raykosocool <rayko200@gmail.com>
2023-02-14 23:40:00 +0800

    test: 💍 Better CSS / Javascript(Not done)

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const todoInput = document.querySelector(".todo-input");
 const todoBtn = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
-document.addEventListener("DOMContentLoaded", LoadDOM);
-document.addEventListener("keypress", addTodoKey);
+todoBtn.addEventListener("keypress", addTodoKey);
 todoBtn.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
 function addTodoKey(event) {
   if (event.which === 13) {
@@ -14,26 +14,44 @@ function addTodoKey(event) {
 
 function addTodo(event) {
   event.preventDefault();
-  const TextInput = document.querySelector(".todo-input").value;
-  localStorage.setItem("Todos", TextInput);
+  const TextInput = todoInput.value;
+  Savelocal(TextInput);
   const newTodo = document.createElement("li");
   newTodo.innerText = todoInput.value;
   newTodo.classList.add("todo-item");
-  todoList.appendChild(newTodo);
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todos");
+  todoDiv.appendChild(newTodo);
+  todoList.appendChild(todoDiv);
   todoInput.value = "";
 
   const CompleteBtn = document.createElement("button");
   CompleteBtn.innerHTML = '<i class="fas fa-check-circle">';
-  CompleteBtn.classList.add("com-btn");
-  todoList.appendChild(CompleteBtn);
+  CompleteBtn.classList.add("combtn");
+  todoDiv.appendChild(CompleteBtn);
 
   const TrashBtn = document.createElement("button");
   TrashBtn.innerHTML = '<i class="fas fa-trash">';
-  TrashBtn.classList.add("trash-btn");
-  todoList.appendChild(TrashBtn);
-  console.log(todoList);
+  TrashBtn.classList.add("trashbtn");
+  todoDiv.appendChild(TrashBtn);
 }
 
-function LoadDOM(event) {
-  console.log("DOM!");
+function deleteCheck(event) {
+  const target = event.target;
+
+  if (target.classList[0] === "trashbtn") {
+    target.parentNode.remove();
+    localStorage.removeItem("Todos");
+  }
+}
+
+function Savelocal(todo) {
+  let t;
+  if (localStorage.getItem("Todos") === null) {
+    t = [];
+  } else {
+    t = JSON.parse(localStorage.getItem("Todos"));
+  }
+  t.push(todo);
+  localStorage.setItem("Todos", JSON.stringify(t));
 }
